6e0403a16a81d131d034b803c4de255f3bdebfcd
Giluk Vladimir <giluk.vs@gmail.com>
2022-10-21 11:25:07 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let numb = 5;
+const leftborderwidth = 1;
+
+numb = 10;
+console.log(numb);
+
+console.log("небо" + 7);
+
+// console.log(something);
+
+let stock;
+console.log(stock);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: true
+};
+
+console.log(obj.isMarried);
+
+const arr = ["a", "b", "c"];
+const arrObj = {
+    0: "a",
+    1: "Б",
+    sh: "c"
+}
+
+//arrObj.b = 1234;
+arrObj["b"] = 1234;
+
+console.log(arrObj["b"]);
+console.log(arrObj.b);
+
+
+
+console.log(arrObj[0]);
+console.log(arrObj[1]);
+console.log(arrObj.sh);
---
+++ script_001.js
@@ -0,0 +1,38 @@
+"use strict"
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = 'Ivan';
+//const answer = prompt('Вам есть 18?', '');
+//console.log(answer + 5);
+//alert(`Привет, ${answer}`);
+
+//const answers = [];
+
+//answers[0] = prompt('Как вас зовут?', '');
+//answers[1] = prompt('Какой у вас рост?', '');
+//answers[2] = prompt('Сколько вам лет?', '');
+
+//console.log(typeof(answers));
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 * 5 == 34 % 12);
+
+const isChecked = false,
+    isClose = false,
+    isGooded = true;
+
+console.log(isChecked || isClose || !isGooded);
